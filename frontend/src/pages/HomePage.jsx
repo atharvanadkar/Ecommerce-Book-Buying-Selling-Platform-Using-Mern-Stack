ee00f855ebefc401d43f153a4147e710379e7869
atharvanadkar <atharvanadkar2004@gmail.com>
2026-08-19 03:27:47 +0530

    Remove duplicate ProductBanner, keep only original carousel

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,9 @@
 import React, { useEffect } from 'react'
 import { Navbar } from '../features/navigation/components/Navbar'
 import { ProductList } from '../features/products/components/ProductList'
-import { ProductBanner } from '../features/products/components/ProductBanner'
 import { resetAddressStatus, selectAddressStatus } from '../features/address/AddressSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { Footer } from '../features/footer/Footer'
-
-// Banner images - Book themed
-const bannerImages = [
-    'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?w=1200&h=400&fit=crop&q=80',
-    'https://images.unsplash.com/photo-1507842217343-583bb7270b66?w=1200&h=400&fit=crop&q=80',
-    'https://images.unsplash.com/photo-1495446815901-a7297e633e8d?w=1200&h=400&fit=crop&q=80',
-    'https://images.unsplash.com/photo-1507842217343-583bb7270b66?w=1200&h=400&fit=crop&q=80'
-]
 
 export const HomePage = () => {
 
@@ -28,7 +19,6 @@ export const HomePage = () => {
   return (
     <>
       <Navbar isProductList={true} />
-      <ProductBanner images={bannerImages} />
       <ProductList />
       <Footer />
     </>
